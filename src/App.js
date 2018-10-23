805985491478c4c2924762a6aa1cc7737b9d9ee3
Eran Ovadia <eran@meetsnappy.com>
2018-10-24 00:23:49 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,24 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const App = () => <Amount />;
+const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
+const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
+
+class Amount extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amount: 0,
+    };
+  }
+
+  onIncrement = () => {
+    this.setState(state => ({ amount: state.amount + 1 }));
+  };
+
+  onDecrement = () => {
+    this.setState(state => ({ amount: state.amount - 1 }));
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Currency Converter</h1>
         </header>
+        <div className="App-body">
+          <button type="button" onClick={this.onIncrement}> + </button>
+          <button type="button" onClick={this.onDecrement}> - </button>
+          <span>US Dollar: {this.state.amount} </span>
+          <Euro amount={this.state.amount} />
+          <Pound amount={this.state.amount} />
+        </div>
       </div>
     );
   }
